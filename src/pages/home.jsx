951f423c8668935b4561892256eb2c2cc5748953
vilma cupara soliz <vilmacuparasoliz66@gmail.com>
2026-07-01 23:04:42 -0400

    pantalla de inicio

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,11 @@
-import miau1 from '../assets/img/miau1.png'
+import vsiaf from '../assets/img/vsiaf.PNG'
+import '../styles/home.css'
 
 function Home() {
   return (
     <>
       <div className="home">
-        <h1>BIEMBENIDO A ESTA PAGINA </h1>
-        <p>This is the home page of our application.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel sapien eget nunc efficitur commodo. Sed at ligula a nunc efficitur tincidunt. Curabitur ac odio id metus bibendum fermentum.</p>
-        <img src={miau1} alt="Placeholder" />
+          <img src={vsiaf} className="imagen" alt="Placeholder" />
       </div>
     </>
   )
