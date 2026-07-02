0ef9a23ae62a65d34c80a9243b19768361fbb860
rulyana-arriola-mamanillo <rarriolamamanillo18@gmail.com>
2026-07-02 07:00:29 -0400

    Revert "Actualizando  transferencia"

--- src/pages/transferencia.jsx
+++ src/pages/transferencia.jsx
@@ -1,8 +1,10 @@
-import ShowTransferencia from '../components/transferencia/show';
-import '../styles/transferencia.css';
-
 function Transferencia() {
-  return <ShowTransferencia />;
+  return (
+    <div className="transferencia-container">
+      <h1>Transferencia</h1>
+      <p>Gestión de transferencias administrativas.</p>
+    </div>
+  );
 }
 
 export default Transferencia;
